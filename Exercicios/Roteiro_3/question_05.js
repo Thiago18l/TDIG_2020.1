13aec133fc48bb3f90bb55021a4bb097346be16e
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-11-13 10:16:32 -0300

    chore: add question 05 from script 3

---
+++ Exercicios/Roteiro_3/question_05.js
@@ -0,0 +1,46 @@
+
+
+const my_arr = ['joazin', 'thiagin', 'maria', 'peneira', 'allan']
+
+
+const verify = function (array) {
+    array.forEach(element => {
+        if (typeof element === "number") {
+            throw new Error("Existe um número no array")
+        }
+    });
+}
+
+async function colocarTodasPalavrasEmMaiusculo(array) {
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            verify(array)
+            resolve(array.map(e => {
+                if (typeof e === "string") {
+                    return e.toUpperCase()
+                }
+            }))
+        }, 3000)
+    })
+    let result = await promise
+    return result
+}
+
+async function ordenarPalavras(array) {
+    let promise = new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            array.forEach(element => {
+                if (typeof element === "number") {
+                    reject(new Error(`FUDEU TEM UM NÚMERO`))  
+                }
+            });
+            resolve(array.sort())
+            
+        }, 4000)
+    })
+    let result = await promise
+    return result
+}
+
+colocarTodasPalavrasEmMaiusculo(my_arr).then(console.log).catch(console.error)
+ordenarPalavras(my_arr).then(console.log).catch(console.error)
